777d27070fafdcfa10a645fab422846fbb572355
Christian Ramirez <christian.r0602@gmail.com>
2022-08-26 08:39:59 -0500

    minToFront

--- script.js
+++ script.js
@@ -152,3 +152,34 @@
 // }
 
 // console.log(removeAt([1,3,4,2,4], 1))
+
+
+// Min to Front
+// Given an array of comparable values, move the lowest element to array’s front, shifting backward any elements previously ahead of it. Do not otherwise change the array’s order. Given [4,2,1,3,5], change it to [1,4,2,3,5] and return it. As always, do this without using built-in functions.
+
+function minToFront(arr){
+  let minVal = arr[0]
+  let minIndex = 0
+  // loop through array to find the lowest value
+  for (let i = 1; i < arr.length; i++){
+    if (arr[i] < minVal){
+      minVal = arr[i]
+      // console.log(minVal)
+      minIndex = i
+    }
+  }
+  // when you find the lowest value, move it to the front
+  // Once I find the lowest value, I want to rearrange the array, so that the minValue goes to the front of the array and all other elements shift to the right
+  for (let i = minIndex; i > 0; i--){
+    let temp = arr[i]
+    // console.log(temp)
+    arr[i] = arr[i - 1]
+    arr[i - 1] = temp
+    
+  }
+ 
+  // the elements that were originally in front of the lowest value, need to be shifted to the right one
+  return arr
+}
+
+console.log(minToFront([2,4,6,3,1]))
