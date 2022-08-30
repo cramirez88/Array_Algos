7c5e672084dcef16a6a7f9f2f8415adf3614630a
Christian Ramirez <christian.r0602@gmail.com>
2022-08-30 07:20:46 -0500

    duplicates with one array

--- script.js
+++ script.js
@@ -242,17 +242,53 @@
 // array – move values within the array that you are given. As always, do not use built-in array functions such as splice().
 
 
-function reverseArray(arr){
+// function reverseArray(arr){
   
-  for (let i = 0; i < arr.length/2; i++){
-    let temp = arr[i]
-    arr[i] = arr[arr.length - 1 - i]
-    arr[arr.length - 1 - i] = temp
-  }
-  return arr
-}
+//   for (let i = 0; i < arr.length/2; i++){
+//     let temp = arr[i]
+//     arr[i] = arr[arr.length - 1 - i]
+//     arr[arr.length - 1 - i] = temp
+//   }
+//   return arr
+// }
 
-console.log(reverseArray([1,2,3,4,5,6]))
+// console.log(reverseArray([1,2,3,4,5,6]))
+
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+// var removeDuplicates = function(nums) {
+//   //     loop through nums array and look for duplicates
+//       let duplicates = 0
+//       for (let i = 0; i < nums.length; i++){
+//           if (nums[i] === nums[i + 1]){
+//               duplicates++
+//           }else {
+//               nums[i - duplicates] = nums[i]
+//           }
+           
+//       }
+//        return nums.length = nums.length - duplicates
+//   };
+
+
+//   console.log(removeDuplicates([1,1,1,1,1,1,3,4,2,5]))
+
+
+
+
+
+
+
+
+
 
 
 
